624fdd743023f0f7e0c13953902f5c8bf5c1013f
Manuel Pichardo <mlpichardo1@gmail.com>
2018-04-13 03:11:23 +0000

    completed caesar

--- pset2/caesar.c
+++ pset2/caesar.c
@@ -4,44 +4,63 @@
 #include <ctype.h>
 #include <stdlib.h>
 
-int main()
+int main(int argc, string argv[])
 {
 
-    string input;
-    int i, key;
+    string input; // store user input
 
-    do
+    if (argc != 2)
     {
-        // Prompt user for Name
-        input = get_string("Enter a message to encrypt: ");
-        key = get_int("Enter key: ");
+        printf("Usage: enter key\n"); // Alert user if key is not entered properly
+        return 1;
     }
-    while (input < 0);
 
-    char encrpyt;
-    // iterate through user input
-      for(i = 0; input[i] != '\0'; ++i){
-        encrpyt = input[i];
+    int key = atoi(argv[1]); // store user key number
 
-        if(encrpyt >= 'a' && ch <= 'z'){
-            encrpyt = encrpyt + key;
+    if (key < 0) // check if the integer is non-negative
+    {
+        printf("Try again\n");
+        return 1;
+    }
 
-            if(encrpyt > 'z'){
-                encrpyt = encrpyt - 'z' + 'a' - 1;
+    else
+    {
+
+        input = get_string("Enter a message to encrypt: "); // Prompt user for input
+        printf("ciphertext: ");
+
+        for (int i = 0, n = strlen(input); i < n; i++)
+        {
+            char cipher = input[i];
+            //check if the letter is uppercase or lowercase then convert
+
+            if islower(input[i])
+            {
+                cipher = (((input[i] + key) - 97) % 26) + 97;
             }
 
-            input[i] = encrpyt;
-        }
-        else if(encrpyt >= 'A' && encrpyt <= 'Z'){
-            encrpyt = encrpyt + key;
-
-            if(encrpyt > 'Z'){
-                encrpyt = encrpyt - 'Z' + 'A' - 1;
+            if isupper(input[i])
+            {
+                cipher = (((input[i] + key) - 65) % 26) + 65;
             }
 
-            input[i] = encrpyt;
+            //if neither then just print whats left
+            if (isalpha(input[i]))
+            {
+                input[i] = cipher;
+                printf("%c", cipher);
+            }
+
+            else
+            {
+                printf("%c", cipher);
+            }
+
         }
+
+        printf("\n");
+        return 0;
+
     }
-    printf("Encrypted message: %s", encrpyt);
-    printf("\n");
+
 }
