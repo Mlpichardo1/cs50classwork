d4aa10402d9f2167adeb0ad8a510cfd47f19013c
Manuel Pichardo <mlpichardo1@gmail.com>
2018-04-17 00:58:43 +0000

    entered duration function

--- pset3/music/helpers.c
+++ pset3/music/helpers.c
@@ -7,7 +7,37 @@
 // Converts a fraction formatted as X/Y to eighths
 int duration(string fraction)
 {
-    // TODO
+    int x = fraction[0];
+    int y = fracrion[2];
+
+    if (x == '1')
+    {
+        if (y == '8')
+        {
+            return 1;
+        }
+        else if (y == '4')
+        {
+            return 2;
+        }
+        else if (y == '2')
+        {
+            return 4;
+        }
+        else if (y == '1')
+        {
+            return 8;
+        }
+    }
+    else if (x == '3' && y == '8')
+    {
+        return 3;
+    }
+    else
+    {
+        return 0;
+    }
+    return 0;
 }
 
 // Calculates frequency (in Hz) of a note
@@ -19,5 +49,9 @@ int frequency(string note)
 // Determines whether a string represents a rest
 bool is_rest(string s)
 {
-    // TODO
+    if (s[0] == "\0") // Check if s is blank
+    {
+        return true
+    }
+    return false;
 }
