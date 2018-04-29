633d616f4b90ee26c636be2878e255a8ac379ea8
Manuel Pichardo <mlpichardo1@gmail.com>
2018-04-29 04:18:30 +0000

    completed resize

--- pset4/resize/resize.c
+++ pset4/resize/resize.c
@@ -6,22 +6,30 @@
 int main(int argc, char *argv[])
 {
     // ensure proper usage
-    if (argc != 3)
+    if (argc != 4)
     {
         fprintf(stderr, "Usage: copy infile outfile\n");
         return 1;
     }
 
     // remember filenames
-    char *infile = argv[1];
-    char *outfile = argv[2];
+    int n = atoi(argv[1]); // accept and Integer for input
+    char *infile = argv[2];
+    char *outfile = argv[3];
+
+    // check for value of n 1-100
+    if (n < 0 || n > 100)
+    {
+        printf("Enter a number between 0 and 100.");
+        return 2;
+    }
 
     // open input file
     FILE *inptr = fopen(infile, "r");
     if (inptr == NULL)
     {
         fprintf(stderr, "Could not open %s.\n", infile);
-        return 2;
+        return 3;
     }
 
     // open output file
@@ -30,7 +38,7 @@ int main(int argc, char *argv[])
     {
         fclose(inptr);
         fprintf(stderr, "Could not create %s.\n", outfile);
-        return 3;
+        return 4;
     }
 
     // read infile's BITMAPFILEHEADER
@@ -48,41 +56,63 @@ int main(int argc, char *argv[])
         fclose(outptr);
         fclose(inptr);
         fprintf(stderr, "Unsupported file format.\n");
-        return 4;
+        return 5;
     }
 
-    // write outfile's BITMAPFILEHEADER
-    fwrite(&bf, sizeof(BITMAPFILEHEADER), 1, outptr);
-
-    // write outfile's BITMAPINFOHEADER
-    fwrite(&bi, sizeof(BITMAPINFOHEADER), 1, outptr);
+    // update new file headers
+    BITMAPFILEHEADER bfResize = bf;
+    BITMAPINFOHEADER biResize = bi;
+    biResize.biWidth *= n;
+    biResize.biHeight *= n;
 
     // determine padding for scanlines
     int padding = (4 - (bi.biWidth * sizeof(RGBTRIPLE)) % 4) % 4;
+    int paddingResize = (4 - (biResize.biWidth * sizeof(RGBTRIPLE)) % 4) % 4;
+
+    // Re Size image
+    biResize.biSizeImage = (sizeof(RGBTRIPLE) * biResize.biWidth + paddingResize) * abs(biResize.biHeight);
+    bfResize.bfSize = biResize.biSizeImage + sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER);
+
+    // write outfile's BITMAPFILEHEADER
+    fwrite(&bfResize, sizeof(BITMAPFILEHEADER), 1, outptr);
+
+    // write outfile's BITMAPINFOHEADER
+    fwrite(&biResize, sizeof(BITMAPINFOHEADER), 1, outptr);
 
     // iterate over infile's scanlines
     for (int i = 0, biHeight = abs(bi.biHeight); i < biHeight; i++)
     {
-        // iterate over pixels in scanline
-        for (int j = 0; j < bi.biWidth; j++)
+        for (int vertical = 0; vertical < n; vertical++)
         {
-            // temporary storage
-            RGBTRIPLE triple;
+            // iterate over pixels in scanline
+            for (int j = 0; j < bi.biWidth; j++)
+            {
+                // temporary storage
+                RGBTRIPLE triple;
 
-            // read RGB triple from infile
-            fread(&triple, sizeof(RGBTRIPLE), 1, inptr);
+                // read RGB triple from infile
+                fread(&triple, sizeof(RGBTRIPLE), 1, inptr);
 
-            // write RGB triple to outfile
-            fwrite(&triple, sizeof(RGBTRIPLE), 1, outptr);
-        }
+                for (int horizontal = 0; horizontal < n; horizontal++)
+                {
+                    // write RGB triple to outfile//  WIDTH is taken
+                    fwrite(&triple, sizeof(RGBTRIPLE), 1, outptr);
+                }
+            }
+            // skip over padding, if any
+            fseek(inptr, padding, SEEK_CUR);
 
-        // skip over padding, if any
-        fseek(inptr, padding, SEEK_CUR);
+            // then add it back (to demonstrate how) Resized Padding
+            for (int k = 0; k < paddingResize; k++)
+            {
+                fputc(0x00, outptr);
+            }
 
-        // then add it back (to demonstrate how)
-        for (int k = 0; k < padding; k++)
-        {
-            fputc(0x00, outptr);
+            if (vertical < n - 1)
+            {
+                fseek(inptr, -(bi.biWidth * 3 + padding), SEEK_CUR);
+            }
+
         }
     }
 
